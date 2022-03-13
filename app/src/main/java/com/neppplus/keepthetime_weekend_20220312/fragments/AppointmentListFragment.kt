f4b846fc698119c22beff8c15f3448cc0b5f94af
nepp.plus <nepp.plus@gmail.com>
2022-03-13 14:05:16 +0900

    약속목록 프래그먼트 - BaseFragment 상속받아 완성

--- app/src/main/java/com/neppplus/keepthetime_weekend_20220312/fragments/AppointmentListFragment.kt
+++ app/src/main/java/com/neppplus/keepthetime_weekend_20220312/fragments/AppointmentListFragment.kt
@@ -5,8 +5,32 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.neppplus.keepthetime_weekend_20220312.R
 
 class AppointmentListFragment : BaseFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return  inflater.inflate(R.layout.fragment_appointment_list, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        setupEvents()
+        setValues()
+    }
+
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
 
 
 }
