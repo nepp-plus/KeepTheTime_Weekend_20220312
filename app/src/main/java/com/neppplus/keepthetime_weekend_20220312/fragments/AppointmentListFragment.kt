774b7fc204e634abed497eacf38d523210257740
nepp.plus <nepp.plus@gmail.com>
2022-03-13 14:03:06 +0900

    프래그먼트들의 공통기능 - BaseFragment 클래스 추가  - mContext 변수 : requireContext() 대체 용도  - apiList 변수 : 서버통신을 쉽게 활용.  - 추상 함수 2개 => setupEvents / setValues : 에러 해결 도구로 자동완성

---
+++ app/src/main/java/com/neppplus/keepthetime_weekend_20220312/fragments/AppointmentListFragment.kt
@@ -0,0 +1,12 @@
+package com.neppplus.keepthetime_weekend_20220312.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class AppointmentListFragment : BaseFragment() {
+
+
+}
