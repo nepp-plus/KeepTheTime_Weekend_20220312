52e0960fc1caf67603c6bf2ba6edd61be287ae0b
nepp.plus <nepp.plus@gmail.com>
2022-03-13 13:37:45 +0900

    토큰값 조회 / 저장 기능 추가

--- app/src/main/java/com/neppplus/keepthetime_weekend_20220312/utils/ContextUtil.kt
+++ app/src/main/java/com/neppplus/keepthetime_weekend_20220312/utils/ContextUtil.kt
@@ -14,6 +14,7 @@ class ContextUtil {
 
 //        저장할 항목의 이름. (조회할때도 같은이름 사용)
         private val AUTO_LOGIN = "AUTO_LOGIN"
+        private val TOKEN = "TOKEN"
 
 //        해당항목에 저장 기능 / 조회 기능
 
@@ -37,6 +38,22 @@ class ContextUtil {
 
 //            저장된 자동로그인 데이터가 없다면 내보내줄 기본값도 설정해야함.
             return  pref.getBoolean( AUTO_LOGIN, false )
+
+        }
+
+
+//        TOKEN 항목에, String값을 저장 / 조회 기능
+//          - 메모장이름은 이미 만들어져있다. 항목명은 X. setter / getter 도 X.
+
+        fun setToken( context: Context, token: String ) {
+            val pref = context.getSharedPreferences( prefName, Context.MODE_PRIVATE )
+            pref.edit().putString(TOKEN,  token).apply()
+        }
+
+        fun getToken( context: Context ) : String {
+
+            val pref = context.getSharedPreferences( prefName, Context.MODE_PRIVATE )
+            return pref.getString(TOKEN, "")!!
 
         }
 
