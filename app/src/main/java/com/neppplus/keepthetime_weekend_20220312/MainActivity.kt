6f8832c1c1df60328d3875cff01f48bf1fe44c51
nepp.plus <nepp.plus@gmail.com>
2022-03-13 13:54:54 +0900

    메인화면 구조 변경중  - 두개의 프래그먼트 / 뷰페이저 / 탭레이아웃

--- app/src/main/java/com/neppplus/keepthetime_weekend_20220312/MainActivity.kt
+++ app/src/main/java/com/neppplus/keepthetime_weekend_20220312/MainActivity.kt
@@ -27,27 +27,6 @@ class MainActivity : BaseActivity() {
 
     override fun setValues() {
 
-//        연습용 (임시 기능)
-//        로그인 한 사람의 정보를 받아오자. (서버에서) => GET - /user 기능.
-//        닉네임을 추출해서 > 텍스트뷰에 반영
-
-        apiList.getRequestMyInfo( ContextUtil.getToken(mContext) ).enqueue( object : Callback<BasicResponse> {
-            override fun onResponse(call: Call<BasicResponse>, response: Response<BasicResponse>) {
-
-                if (response.isSuccessful) {
-                    val br = response.body()!!
-
-                    binding.txtMyNickname.text =  br.data.user.nick_name
-
-                }
-
-            }
-
-            override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
-
-            }
-
-        } )
 
     }
 }
