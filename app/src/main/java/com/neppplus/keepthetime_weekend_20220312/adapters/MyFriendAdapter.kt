7ac80e3d03c4f4f6a95dabb1adc1b47872c6cb4f
nepp.plus <nepp.plus@gmail.com>
2022-03-13 16:12:48 +0900

    getView 함수 기초 작업

--- app/src/main/java/com/neppplus/keepthetime_weekend_20220312/adapters/MyFriendAdapter.kt
+++ app/src/main/java/com/neppplus/keepthetime_weekend_20220312/adapters/MyFriendAdapter.kt
@@ -1,7 +1,11 @@
 package com.neppplus.keepthetime_weekend_20220312.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import com.neppplus.keepthetime_weekend_20220312.R
 import com.neppplus.keepthetime_weekend_20220312.datas.UserData
 
 class MyFriendAdapter(
@@ -9,4 +13,20 @@ class MyFriendAdapter(
     resId: Int,
     val mList: List<UserData>  // 자바 언어의 다형성 개념 활용 예제.
 ) : ArrayAdapter<UserData>(mContext, resId, mList) {
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var tempRow = convertView
+
+        if ( tempRow == null ) {
+            tempRow = LayoutInflater.from(mContext).inflate(R.layout.friend_list_item, null)
+        }
+
+        val row = tempRow!!
+
+
+        return row
+
+    }
+
 }
