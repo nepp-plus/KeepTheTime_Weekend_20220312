d759d93fea603047ef3827ac6fc22b1775b9b5e6
nepp.plus <nepp.plus@gmail.com>
2022-03-13 11:18:04 +0900

    서버가 주는 data: { }는 별도의 클래스를 만들어서, 그 모양을 표현

--- app/src/main/java/com/neppplus/keepthetime_weekend_20220312/datas/BasicResponse.kt
+++ app/src/main/java/com/neppplus/keepthetime_weekend_20220312/datas/BasicResponse.kt
@@ -3,5 +3,6 @@ package com.neppplus.keepthetime_weekend_20220312.datas
 class BasicResponse(
     val code: Int,
     val message: String,
+    val data: DataResponse,
 ) {
 }
---
+++ app/src/main/java/com/neppplus/keepthetime_weekend_20220312/datas/DataResponse.kt
@@ -0,0 +1,7 @@
+package com.neppplus.keepthetime_weekend_20220312.datas
+
+class DataResponse(
+//    val user: ?? 현재는 skip.
+    val token: String,
+) {
+}
