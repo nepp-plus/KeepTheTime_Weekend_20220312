04a2261494f742ac4750da3f78a90f4dc0f7b8b9
nepp.plus <nepp.plus@gmail.com>
2022-03-13 15:15:15 +0900

    자동로그인 기능 구현 중  - 내 정보를 임시로 무조건 잘 불러와졌다고 취급.

--- app/src/main/java/com/neppplus/keepthetime_weekend_20220312/SplashActivity.kt
+++ app/src/main/java/com/neppplus/keepthetime_weekend_20220312/SplashActivity.kt
@@ -1,7 +1,11 @@
 package com.neppplus.keepthetime_weekend_20220312
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.neppplus.keepthetime_weekend_20220312.utils.ContextUtil
 
 class SplashActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,6 +20,39 @@ class SplashActivity : BaseActivity() {
     }
 
     override fun setValues() {
+
+//        API로, 토큰값을 이용해 내 정보 조회
+
+
+//        2.5초 후에 내 정보가 불러와졌는지? + 자동로그인을 한다고 했는지? 검사.
+
+        val myHandler = Handler(Looper.getMainLooper())
+
+        myHandler.postDelayed( {
+
+//           자동로그인 한다고 했는지?
+            val isAutoLogin =  ContextUtil.getAutoLogin(mContext)
+
+//            내 정보가 잘 불러와졌는지?
+            val isMyInfoOk = true // 임시로 true
+
+//            둘다 통과하면? 메인으로, 하나라도 틀리면? 로그인으로.
+
+            if (isAutoLogin && isMyInfoOk) {
+                val myIntent = Intent(mContext,  MainActivity::class.java)
+                startActivity(myIntent)
+            }
+            else {
+                val myIntent = Intent(mContext, LoginActivity::class.java)
+                startActivity(myIntent)
+            }
+
+//            화면이동 후에는 로딩화면 종료
+
+            finish()
+
+
+        }, 2500 )
 
     }
 }
