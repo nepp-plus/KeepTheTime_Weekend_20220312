fd236d6f5a59bfbc23a55d9c1f8323245245dbc0
nepp.plus <nepp.plus@gmail.com>
2022-03-13 16:15:06 +0900

    실제 친구 목록을 채울 ArrayList 생성

--- app/src/main/java/com/neppplus/keepthetime_weekend_20220312/ManageFriendListActivity.kt
+++ app/src/main/java/com/neppplus/keepthetime_weekend_20220312/ManageFriendListActivity.kt
@@ -4,10 +4,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.neppplus.keepthetime_weekend_20220312.databinding.ActivityManageFriendListBinding
+import com.neppplus.keepthetime_weekend_20220312.datas.UserData
 
 class ManageFriendListActivity : BaseActivity() {
 
     lateinit var binding: ActivityManageFriendListBinding
+
+//    내 친구목록을 담아줄 그릇
+    val mMyFriendList = ArrayList<UserData>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +25,9 @@ class ManageFriendListActivity : BaseActivity() {
     }
 
     override fun setValues() {
+
+//        내 친구목록에 데이터 채우기.
+//         => 서버가 실제로 내려주는 친구목록을 채워보자. (API 통신과 결합)
 
     }
 }
