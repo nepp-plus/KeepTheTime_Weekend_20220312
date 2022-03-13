c41128b438c70da8a9ba001464860a8676c34c51
nepp.plus <nepp.plus@gmail.com>
2022-03-13 10:44:03 +0900

    서버에 실제로 중복확인 요청 > 응답이 성공으로 돌아왔을때에 대한 코딩

--- app/src/main/java/com/neppplus/keepthetime_weekend_20220312/SignUpActivity.kt
+++ app/src/main/java/com/neppplus/keepthetime_weekend_20220312/SignUpActivity.kt
@@ -28,6 +28,33 @@ class SignUpActivity : BaseActivity() {
 //            입력된 이메일 추출 > 서버의 중복확인 기능에 물어보자.
             val inputEmail = binding.edtEmail.text.toString()
 
+//            실제로 API 기능 사용.
+            apiList.getRequestDuplicatedCheck("EMAIL", inputEmail).enqueue(object : Callback<BasicResponse> {
+                override fun onResponse(
+                    call: Call<BasicResponse>,
+                    response: Response<BasicResponse>
+                ) {
+//                    response 변수가, 서버의 응답을 들고 있다. => 그 내부 분석.
+//                     1) 최종 성공? - 모든 중복을 피해서, 사용해도 되는 경우만.
+//                     2) 본문에 들어있는 내용?
+
+                    if (response.isSuccessful) {
+
+                        val br = response.body()!!
+
+                        Toast.makeText(mContext, br.message, Toast.LENGTH_SHORT).show()
+
+                    }
+
+                }
+
+                override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
+
+                }
+
+
+            } )
+
         }
 
         binding.btnSignUp.setOnClickListener {
