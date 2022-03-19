08377af4d02eb695990b88e379357c78cdf7d125
nepp.plus <nepp.plus@gmail.com>
2022-03-19 11:39:30 +0900

    리싸이클러뷰의 어댑터 작업중  - inner class로 ViewHolder 상속 받은 클래스 제작

---
+++ app/src/main/java/com/neppplus/keepthetime_weekend_20220312/adapters/MyFriendRecyclerAdapter.kt
@@ -0,0 +1,14 @@
+package com.neppplus.keepthetime_weekend_20220312.adapters
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class MyFriendRecyclerAdapter : RecyclerView.Adapter<>() {
+
+//    클래스 내부의 클래스 (inner class) 제작 > MyFriendRecyclerAdapter가 혼자 사용.
+
+    inner class  MyViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+    }
+
+}
