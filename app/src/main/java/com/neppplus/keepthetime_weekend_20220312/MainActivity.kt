7ef3de055ff4b86895895389d043ffeed2bf2c59
nepp.plus <nepp.plus@gmail.com>
2022-03-19 15:13:15 +0900

    어떤 메뉴 아이템을 골랐냐에 따라서 > 뷰 페이저 페이지 이동 처리.

--- app/src/main/java/com/neppplus/keepthetime_weekend_20220312/MainActivity.kt
+++ app/src/main/java/com/neppplus/keepthetime_weekend_20220312/MainActivity.kt
@@ -27,6 +27,22 @@ class MainActivity : BaseActivity() {
 
     override fun setupEvents() {
 
+//        바텀 네비게이션의 이벤트 처리.
+        binding.mainBottomNav.setOnItemSelectedListener {
+
+//            it 변수 : 선택된 메뉴가 뭔지? 알려줌.
+//            it변수의 id값에 따라, 페이지 이동.
+
+            if (it.itemId == R.id.home) {
+                binding.mainViewPager2.currentItem = 0
+            }
+            else {
+                binding.mainViewPager2.currentItem = 1
+            }
+
+            return@setOnItemSelectedListener true
+        }
+
     }
 
     override fun setValues() {
