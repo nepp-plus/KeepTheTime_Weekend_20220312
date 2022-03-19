4eec07b63260f3f123ccecfe6691c532f56d5f44
nepp.plus <nepp.plus@gmail.com>
2022-03-19 11:14:35 +0900

    친구목록 관리화면의 ListView 제거  - TabLayout / ViewPager 배치 예정

--- app/src/main/java/com/neppplus/keepthetime_weekend_20220312/ManageFriendListActivity.kt
+++ app/src/main/java/com/neppplus/keepthetime_weekend_20220312/ManageFriendListActivity.kt
@@ -15,11 +15,6 @@ class ManageFriendListActivity : BaseActivity() {
 
     lateinit var binding: ActivityManageFriendListBinding
 
-//    내 친구목록을 담아줄 그릇
-    val mMyFriendList = ArrayList<UserData>()
-
-    lateinit var mAdapter: MyFriendAdapter
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_manage_friend_list)
@@ -33,42 +28,7 @@ class ManageFriendListActivity : BaseActivity() {
 
     override fun setValues() {
 
-//        내 친구목록에 데이터 채우기.
-//         => 서버가 실제로 내려주는 친구목록을 채워보자. (API 통신과 결합)
-
-        getMyFriendListFromServer()
-
-        mAdapter = MyFriendAdapter(mContext, R.layout.friend_list_item, mMyFriendList)
-        binding.myFriendListView.adapter = mAdapter
-
     }
 
-    fun getMyFriendListFromServer() {
-
-        apiList.getRequestFriendList(
-            "my"
-        ).enqueue( object : Callback<BasicResponse> {
-            override fun onResponse(call: Call<BasicResponse>, response: Response<BasicResponse>) {
-
-                if (response.isSuccessful) {
-
-                    val br = response.body()!!
-                    mMyFriendList.addAll( br.data.friends ) // 서버가 주는 친구 목록을 > 화면의 ArrayList에 통째로 추가
-
-//                    리스트뷰의 어댑터 설정보다, 목록에 데이터 추가가 더 늦게 이루어질 수도 있다.
-                    mAdapter.notifyDataSetChanged() // 리스트뷰의 내용물 새로고침
-
-                }
-
-            }
-
-            override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
-
-            }
-
-
-        } )
-
-    }
 
 }
