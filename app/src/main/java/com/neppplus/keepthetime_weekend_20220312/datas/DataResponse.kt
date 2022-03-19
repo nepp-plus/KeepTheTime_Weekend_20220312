1d189fec287879cda34b7eb8145dd0c6548a6b4b
nepp.plus <nepp.plus@gmail.com>
2022-03-19 10:47:20 +0900

    서버가 주는 친구 목록 데이터 파싱

--- app/src/main/java/com/neppplus/keepthetime_weekend_20220312/datas/DataResponse.kt
+++ app/src/main/java/com/neppplus/keepthetime_weekend_20220312/datas/DataResponse.kt
@@ -1,7 +1,9 @@
 package com.neppplus.keepthetime_weekend_20220312.datas
 
 class DataResponse(
-    val user: UserData,
+    val user: UserData, // 로그인 / 회원가입 API가 호출되면 사용할 데이터.
     val token: String,
+
+    val friends: List< UserData >, // 친구목록 API가 호출되면 사용할 데이터.
 ) {
 }
