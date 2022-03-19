511e725bd404d9612e2c07de764adf608ccd5cbc
nepp.plus <nepp.plus@gmail.com>
2022-03-19 12:03:30 +0900

    getItemCount, onCreateViewHolder 작성

--- app/src/main/java/com/neppplus/keepthetime_weekend_20220312/adapters/MyFriendRecyclerAdapter.kt
+++ app/src/main/java/com/neppplus/keepthetime_weekend_20220312/adapters/MyFriendRecyclerAdapter.kt
@@ -1,9 +1,11 @@
 package com.neppplus.keepthetime_weekend_20220312.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.neppplus.keepthetime_weekend_20220312.R
 import com.neppplus.keepthetime_weekend_20220312.datas.UserData
 
 class MyFriendRecyclerAdapter(
@@ -19,14 +21,15 @@ class MyFriendRecyclerAdapter(
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
 
+        val view = LayoutInflater.from(mContext).inflate(R.layout.friend_list_item, parent, false)
+        return  MyViewHolder( view )
+
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
 
     }
 
-    override fun getItemCount(): Int {
-
-    }
+    override fun getItemCount() = mList.size  // 목록의 갯수가 리턴.
 
 }
