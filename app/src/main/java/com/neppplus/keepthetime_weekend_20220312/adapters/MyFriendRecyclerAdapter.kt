b7c0fafbe55b1b7826cb3bc4e237dcf2b0dfe98b
nepp.plus <nepp.plus@gmail.com>
2022-03-19 12:33:08 +0900

    뷰홀더 inner class의 멤버변수 / 함수 추가, 어댑터의 onBindViewHolder에서 활용

--- app/src/main/java/com/neppplus/keepthetime_weekend_20220312/adapters/MyFriendRecyclerAdapter.kt
+++ app/src/main/java/com/neppplus/keepthetime_weekend_20220312/adapters/MyFriendRecyclerAdapter.kt
@@ -4,7 +4,10 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.neppplus.keepthetime_weekend_20220312.R
 import com.neppplus.keepthetime_weekend_20220312.datas.UserData
 
@@ -17,6 +20,21 @@ class MyFriendRecyclerAdapter(
 
     inner class  MyViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
+//        멤버변수로, view 변수 내부에서 실제 사용할 UI들을 가져와서 담아두자.
+        val imgProfile = view.findViewById<ImageView>(R.id.imgProfile)
+        val txtNickname = view.findViewById<TextView>(R.id.txtNickname)
+        val txtEmail = view.findViewById<TextView>(R.id.txtEmail)
+
+//        함수로, 실 데이터를 넘겨받아서, 멤버변수의 UI태그들과 결합하는 기능 추가.
+
+        fun bind( data: UserData )  {
+            txtNickname.text = data.nick_name
+            txtEmail.text = data.email
+
+            Glide.with(mContext).load(data.profile_img).into(imgProfile)
+
+        }
+
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
@@ -27,6 +45,12 @@ class MyFriendRecyclerAdapter(
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+//        실제 출력할 데이터
+
+        val data = mList[position]
+
+//        MyViewHolder도 일종의 클래스 : 멤버변수 / 함수를 가지고 있을 수 있다. => 활용하자.
+        holder.bind( data )
 
     }
 
