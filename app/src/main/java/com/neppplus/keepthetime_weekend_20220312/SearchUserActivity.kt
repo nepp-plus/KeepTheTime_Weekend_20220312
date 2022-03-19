b1600923cafe7c353341104e4acc9a77004bb97c
nepp.plus <nepp.plus@gmail.com>
2022-03-19 12:49:11 +0900

    검색 화면 추가  - 데이터바인딩 / BaseActivity 상속 처리

---
+++ app/src/main/java/com/neppplus/keepthetime_weekend_20220312/SearchUserActivity.kt
@@ -0,0 +1,26 @@
+package com.neppplus.keepthetime_weekend_20220312
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.neppplus.keepthetime_weekend_20220312.databinding.ActivitySearchUserBinding
+
+class SearchUserActivity : BaseActivity() {
+
+    lateinit var binding: ActivitySearchUserBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_search_user)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+}
