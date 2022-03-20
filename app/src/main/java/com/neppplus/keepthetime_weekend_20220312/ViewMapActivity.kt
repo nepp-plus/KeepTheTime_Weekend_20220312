e8368065d39d2908de0b3bd94389134fe4981b09
nepp.plus <nepp.plus@gmail.com>
2022-03-20 16:28:13 +0900

    도전과제 주석

--- app/src/main/java/com/neppplus/keepthetime_weekend_20220312/ViewMapActivity.kt
+++ app/src/main/java/com/neppplus/keepthetime_weekend_20220312/ViewMapActivity.kt
@@ -5,6 +5,10 @@ import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.neppplus.keepthetime_weekend_20220312.databinding.ActivityViewMapBinding
 
+// 도전 과제
+// 네이버 지도를 화면 가득 띄우기. (라이브러리는 이미 설치 됨)
+//   - 약속 장소의 좌표로 카메라 이동 / 마커 띄우기
+
 class ViewMapActivity : BaseActivity() {
 
     lateinit var binding: ActivityViewMapBinding
