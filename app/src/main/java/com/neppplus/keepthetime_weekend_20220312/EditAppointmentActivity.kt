6b500a7091e5879a67ab61fe601f571e62c63f8c
nepp.plus <nepp.plus@gmail.com>
2022-03-20 10:47:33 +0900

    날짜 선택 변수 클릭시, 달력 팝업 띄우기  - 월의 동작이 이상하게 돌아감.

--- app/src/main/java/com/neppplus/keepthetime_weekend_20220312/EditAppointmentActivity.kt
+++ app/src/main/java/com/neppplus/keepthetime_weekend_20220312/EditAppointmentActivity.kt
@@ -1,7 +1,10 @@
 package com.neppplus.keepthetime_weekend_20220312
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.DatePicker
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.neppplus.keepthetime_weekend_20220312.databinding.ActivityEditAppointmentBinding
 
@@ -17,6 +20,33 @@ class EditAppointmentActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+
+        binding.txtDate.setOnClickListener {
+
+//            날짜가 선택되면 할 일 저장
+
+            val dsl = object : DatePickerDialog.OnDateSetListener {
+                override fun onDateSet(p0: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+
+//                    year, month, dayOfMonth => 달력을 통해서 선택한 일자 정보.
+                    Toast.makeText(mContext, "${year}년 ${month}월 ${dayOfMonth}일", Toast.LENGTH_SHORT).show()
+
+                }
+
+            }
+
+
+//            실제로 달력 팝업 띄우기.
+
+            val dpd = DatePickerDialog(
+                mContext,
+                dsl,
+                2022,
+                3,
+                20
+            ).show()
+
+        }
 
     }
 
