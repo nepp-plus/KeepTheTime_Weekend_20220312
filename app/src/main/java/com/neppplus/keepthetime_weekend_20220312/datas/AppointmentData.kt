d3c2a84a036e660396e078b007b78d63d52615ce
nepp.plus <nepp.plus@gmail.com>
2022-03-20 15:47:56 +0900

    약속 데이터 클래스 추가  - 멤버변수를, 서버가 주는 데이터를 보고 변수명을 설정. (JSON Parsing)

---
+++ app/src/main/java/com/neppplus/keepthetime_weekend_20220312/datas/AppointmentData.kt
@@ -0,0 +1,16 @@
+package com.neppplus.keepthetime_weekend_20220312.datas
+
+class AppointmentData(
+    val id: Int,
+    val user_id: Int,
+    val title: String,
+    val datetime: String, // 실제 내용은 약속 일시. 추가 가공 예정
+    val start_place: String,
+    val start_latitude: Double,
+    val start_longitude: Double,
+    val place: String,
+    val latitude: Double,
+    val longitude: Double,
+
+) {
+}
