7cb8d1cfc076c1fc112a2935b9d10c6b50670968
nepp.plus <nepp.plus@gmail.com>
2022-03-26 17:06:52 +0900

    지도를 클릭하면, 출발지 좌표를 마커로 설정.

--- app/src/main/java/com/neppplus/keepthetime_weekend_20220312/EditStartingPointActivity.kt
+++ app/src/main/java/com/neppplus/keepthetime_weekend_20220312/EditStartingPointActivity.kt
@@ -3,15 +3,23 @@ package com.neppplus.keepthetime_weekend_20220312
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.naver.maps.map.overlay.Marker
 import com.neppplus.keepthetime_weekend_20220312.databinding.ActivityEditStartingPointBinding
 
 class EditStartingPointActivity : BaseActivity() {
 
     lateinit var binding: ActivityEditStartingPointBinding
 
+//    하나의 마커가 계속 위치만 변경. =>  멤버변수
+//    처음에는 안 찍혀있게. (마커가 없게) => null 로 초기값
+
+    var pointMarker : Marker? = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_edit_starting_point)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_edit_starting_point)
         setupEvents()
         setValues()
     }
@@ -34,6 +42,25 @@ class EditStartingPointActivity : BaseActivity() {
     }
 
     override fun setValues() {
+
+        binding.naverMapView.getMapAsync {
+
+            val naverMap = it
+
+            naverMap.setOnMapClickListener { pointF, latLng ->
+
+//                latLng 변수가 클릭된 좌표. => 마커로 표시.
+
+                if (pointMarker == null) {
+                    pointMarker = Marker()
+                }
+
+                pointMarker!!.position =  latLng
+                pointMarker!!.map = naverMap
+
+            }
+
+        }
 
     }
 }
