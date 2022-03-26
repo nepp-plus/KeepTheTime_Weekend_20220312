d152ca03c71176d3adbc1078246cd5e1a7230ad7
nepp.plus <nepp.plus@gmail.com>
2022-03-26 17:02:10 +0900

    입력한 문구 검증

--- app/src/main/java/com/neppplus/keepthetime_weekend_20220312/EditStartingPointActivity.kt
+++ app/src/main/java/com/neppplus/keepthetime_weekend_20220312/EditStartingPointActivity.kt
@@ -2,6 +2,7 @@ package com.neppplus.keepthetime_weekend_20220312
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.neppplus.keepthetime_weekend_20220312.databinding.ActivityEditStartingPointBinding
 
 class EditStartingPointActivity : BaseActivity() {
@@ -16,6 +17,19 @@ class EditStartingPointActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+
+        binding.btnSave.setOnClickListener {
+
+            val inputName = binding.edtStartingPointName.text.toString()
+
+            if (inputName.isEmpty()) {
+
+                Toast.makeText(mContext, "출발지 이름을 입력해주세요.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+
+            }
+
+        }
 
     }
 
