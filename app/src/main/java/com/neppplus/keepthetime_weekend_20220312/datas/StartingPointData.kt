6642b988ea1f06b0f55600abb7bd0d76543ecf9c
nepp.plus <nepp.plus@gmail.com>
2022-03-26 16:04:06 +0900

    서버가 주는 장소 데이터 파싱용 클래스 작업  - JSONObject의 항목들 하나하나를 getInt, getString, getDouble, getBoolean으로 가져와야하는 작업을 대체함.

---
+++ app/src/main/java/com/neppplus/keepthetime_weekend_20220312/datas/StartingPointData.kt
@@ -0,0 +1,13 @@
+package com.neppplus.keepthetime_weekend_20220312.datas
+
+//출발지 데이터 표현 클래스
+
+class StartingPointData(
+    val id: Int,
+    val user_id: Int,
+    val name: String,
+    val latitude: Double,
+    val longitude: Double,
+    val is_primary: Boolean,
+) {
+}
