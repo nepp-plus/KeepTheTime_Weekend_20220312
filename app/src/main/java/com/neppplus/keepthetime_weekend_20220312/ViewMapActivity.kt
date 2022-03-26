41e0b13c2a1bdcb27a6c0da506eed3e94d580a45
nepp.plus <nepp.plus@gmail.com>
2022-03-26 10:45:48 +0900

    도전과제 풀이 시작 - 지도 아이콘이 눌리면, 어떤 약속을 보여줄건지 첨부.

--- app/src/main/java/com/neppplus/keepthetime_weekend_20220312/ViewMapActivity.kt
+++ app/src/main/java/com/neppplus/keepthetime_weekend_20220312/ViewMapActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.neppplus.keepthetime_weekend_20220312.databinding.ActivityViewMapBinding
+import com.neppplus.keepthetime_weekend_20220312.datas.AppointmentData
 
 // 도전 과제
 // 네이버 지도를 화면 가득 띄우기. (라이브러리는 이미 설치 됨)
@@ -13,9 +14,14 @@ class ViewMapActivity : BaseActivity() {
 
     lateinit var binding: ActivityViewMapBinding
 
+    lateinit var mAppointmentData: AppointmentData  // 화면에 넘겨준 약속 자체
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_view_map)
+
+        mAppointmentData = intent.getSerializableExtra("appointment") as AppointmentData
+
         setupEvents()
         setValues()
     }
@@ -25,6 +31,9 @@ class ViewMapActivity : BaseActivity() {
     }
 
     override fun setValues() {
+
+//        약속이름을 화면의 제목으로.
+        txtTitle.text = mAppointmentData.title
 
     }
 }
--- app/src/main/java/com/neppplus/keepthetime_weekend_20220312/datas/AppointmentData.kt
+++ app/src/main/java/com/neppplus/keepthetime_weekend_20220312/datas/AppointmentData.kt
@@ -1,5 +1,7 @@
 package com.neppplus.keepthetime_weekend_20220312.datas
 
+import java.io.Serializable
+
 class AppointmentData(
     val id: Int,
     val user_id: Int,
@@ -12,5 +14,5 @@ class AppointmentData(
     val latitude: Double,
     val longitude: Double,
 
-) {
+) : Serializable {
 }
