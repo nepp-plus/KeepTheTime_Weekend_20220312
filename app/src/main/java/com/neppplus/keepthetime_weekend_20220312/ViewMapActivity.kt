1d2696e813c7206196175205fece54b96f9a984f
nepp.plus <nepp.plus@gmail.com>
2022-03-26 11:11:11 +0900

    문서를 보고, 네이버지도의 정보창 띄우기

--- app/src/main/java/com/neppplus/keepthetime_weekend_20220312/ViewMapActivity.kt
+++ app/src/main/java/com/neppplus/keepthetime_weekend_20220312/ViewMapActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.naver.maps.geometry.LatLng
 import com.naver.maps.map.CameraUpdate
+import com.naver.maps.map.overlay.InfoWindow
 import com.naver.maps.map.overlay.Marker
 import com.neppplus.keepthetime_weekend_20220312.databinding.ActivityViewMapBinding
 import com.neppplus.keepthetime_weekend_20220312.datas.AppointmentData
@@ -60,6 +61,24 @@ class ViewMapActivity : BaseActivity() {
             val marker = Marker()
             marker.position = latLng
             marker.map = naverMap
+
+//            정보창 띄우기
+
+            val infoWindow = InfoWindow()
+
+
+//            object : 추상클래스(생성자) {  }  => 추상 클래스 객체
+//            object : 인터페이스 {   } => 인터페이스는 생성자 X
+            infoWindow.adapter = object : InfoWindow.DefaultTextAdapter(mContext) {
+
+                override fun getText(p0: InfoWindow): CharSequence {
+//                    CharSequence : String으로 생각해도 무방.
+                    return "정보 창 내용"
+                }
+
+            }
+
+            infoWindow.open(marker)
 
 
         }
